2e0592a851e6ce4fb1d7c8e482e569a0b5f4b45d
Ali nadhum <alinadhum1998@gmail.com>
2024-09-28 17:40:13 +0300

    add bootsyrap lib

--- src/utils/Sidebar/Sidebar.jsx
+++ src/utils/Sidebar/Sidebar.jsx
@@ -1,14 +1,18 @@
-
-
-import React from 'react'
-import style from "./sidebar.module.css"
+import React from "react";
+import style from "./sidebar.module.css";
 
 const Sidebar = () => {
   return (
     <div className={style.sidebar}>
-      
+      <div className={style.logo}>
+        <h2>
+          <span>A</span>dmin <span>P</span>anel
+        </h2>
+      </div>
+      <hr />
+     
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
