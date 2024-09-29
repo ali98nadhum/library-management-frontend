2424636f323bc391f7a54fa5b4600131069d2b7e
Ali nadhum <alinadhum1998@gmail.com>
2024-09-29 19:17:31 +0300

    refactor project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage/HomePage";
-import Outline from "./components/outline/Outline";
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Outline />}>
-        <Route index element={<HomePage/>} />
-      </Route>
-    </Routes>
-  </Router>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </Router>
   );
 }
 
