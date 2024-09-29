f1ec75c7b20ebc6693cedc9aae57c2feb76f5c64
Ali nadhum <alinadhum1998@gmail.com>
2024-09-29 20:00:17 +0300

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopHeader from "./utils/TopHeader/TopHeader"
+import Header from "../src/utils/Header/Header"
 
 function App() {
   return (
     <Router>
       <TopHeader/>
+      <Header />
       <Routes>
         <Route></Route>
       </Routes>
---
+++ src/utils/Header/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import style from "./header.module.css";
+
+const Header = () => {
+  return (
+    <header className={style.header}>
+      <div className={style.headerContent}>
+        {/* القائمة الرئيسية في الهيدر */}
+        <nav className={style.navbar}>
+          <ul className={style.navList}>
+            <li className={style.navItem}>
+              <a href="#" className={style.navLink}>
+                الكتب
+              </a>
+            </li>
+            <li className={style.navItem}>
+              <a href="#" className={style.navLink}>
+                إضافة كتاب
+              </a>
+            </li>
+            <li className={style.navItem}>
+              <a href="#" className={style.navLink}>
+                عرض الطلبات
+              </a>
+            </li>
+            <li className={style.navItem}>
+              <a href="#" className={style.navLink}>
+                إضافة طلب
+              </a>
+            </li>
+            <li className={style.navItem}>
+              <a href="#" className={style.navLink}>
+                طباعة فاتورة
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
