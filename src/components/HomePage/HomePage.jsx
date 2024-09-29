d371aa9acb2580118f9dd345f31990b735fd67b1
Ali nadhum <alinadhum1998@gmail.com>
2024-09-29 18:17:06 +0300

    add card for home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React from 'react';
 import style from "./home.module.css";
-// import Sidebar from "../../utils/Sidebar/Sidebar"
 import Container from '../../utils/Container/Container';
+import { FaBookOpen } from "react-icons/fa";
+import { Card, CardContent, Typography, Grid } from '@mui/material';
 
 const HomePage = () => {
-  return (
-    <>
-    <h1>home</h1>
-    </>
-  )
-}
+  const totalBooks = 150; // هنا ضع العدد الإجمالي للكتب
+  const ordersLastWeek = 25; // هنا ضع عدد الطلبات في الأسبوع الماضي
 
-export default HomePage
+  return (
+    <Container>
+      <Grid container spacing={4} justifyContent="center">
+        {/* الكارت الأول */}
+        <Grid item xs={12} md={6}>
+          <Card className={style.card}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                <FaBookOpen /> عدد الكتب الكلي
+              </Typography>
+              <Typography variant="h4" color="text.secondary">
+                {totalBooks}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* الكارت الثاني */}
+        <Grid item xs={12} md={6}>
+          <Card className={style.card}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                الطلبات في آخر أسبوع
+              </Typography>
+              <Typography variant="h4" color="text.secondary">
+                {ordersLastWeek}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default HomePage;
