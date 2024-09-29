57aebd0ff9a259a1a326932c37209e3bfbe7d1c0
Ali nadhum <alinadhum1998@gmail.com>
2024-09-29 12:43:39 +0300

    add sidebar item

--- src/utils/Sidebar/Sidebar.jsx
+++ src/utils/Sidebar/Sidebar.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import style from "./sidebar.module.css";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 const Sidebar = () => {
   return (
@@ -12,7 +10,81 @@ const Sidebar = () => {
         </h2>
       </div>
       <hr />
-      <button className="btn btn-primary">اضغط هنا</button>
+
+      <div className={style.list}>
+        {/* button 1 */}
+        <div className="btn-group dropstart">
+          <button
+            type="button"
+            className={`btn dropdown-toggle ${style["custom-dropdown-button"]}`}
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            الكتب
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <a className={`dropdown-item ${style["custom-dropdown-item"]}`} href="#">
+                عنصر 1
+              </a>
+            </li>
+            <li>
+              <a className={`dropdown-item ${style["custom-dropdown-item"]}`} href="#">
+                عنصر 2
+              </a>
+            </li>
+            <li>
+              <a className={`dropdown-item ${style["custom-dropdown-item"]}`} href="#">
+                عنصر 3
+              </a>
+            </li>
+            <li>
+              <hr className="dropdown-divider" />
+            </li>
+            <li>
+              <a className={`dropdown-item ${style["custom-dropdown-item"]}`} href="#">
+                عنصر 4
+              </a>
+            </li>
+          </ul>
+        </div>
+        {/* button 2 */}
+        <div className="btn-group dropstart">
+          <button
+            type="button"
+            className={`btn dropdown-toggle ${style["custom-dropdown-button"]}`}
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            الطلبات
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <a className={`dropdown-item ${style["custom-dropdown-item"]}`} href="#">
+                عنصر 1
+              </a>
+            </li>
+            <li>
+              <a className={`dropdown-item ${style["custom-dropdown-item"]}`} href="#">
+                عنصر 2
+              </a>
+            </li>
+            <li>
+              <a className={`dropdown-item ${style["custom-dropdown-item"]}`} href="#">
+                عنصر 3
+              </a>
+            </li>
+            <li>
+              <hr className="dropdown-divider" />
+            </li>
+            <li>
+              <a className={`dropdown-item ${style["custom-dropdown-item"]}`} href="#">
+                عنصر 4
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
