8a052f43248328f63057db23f0b9193ebc34ba04
Ali nadhum <alinadhum1998@gmail.com>
2024-09-30 10:34:46 +0300

    add card two

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import style from "./home.module.css";
 import Container from "../../utils/Container/Container";
-import bookIcon from "../../assets/book-icon.png"
+import bookIcon from "../../assets/book-icon.png";
+import orderIcon from "../../assets/order-icon.png";
 
 
 const HomePage = () => {
   return (
     <div className={style.home}>
       <Container>
+        
         <div className={style.topHome}>
+        <div className={style.grid}>
           <div className={style.card}>
             <div className={style.leftSec}>
               <h3>عدد الكتب</h3>
@@ -18,6 +21,16 @@ const HomePage = () => {
                 <img src={bookIcon} alt="book-icon"/>
             </div>
           </div>
+          <div className={style.card}>
+            <div className={style.leftSec}>
+              <h3> طلبات الاسبوع </h3>
+                <p>23</p>
+            </div>
+            <div className={style.rightSec}>
+                <img src={orderIcon} alt="book-icon"/>
+            </div>
+          </div>
+        </div>
         </div>
       </Container>
     </div>
